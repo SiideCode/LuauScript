b4daaee8a3f5543379a665c5f89ab2fb8d823091
SiideCode <94011931+SiideCode@users.noreply.github.com>
2023-12-19 09:59:25 +0300

    maybe make it a type???

--- src/LuauScript/AST/LexerModules/Pos.lua
+++ src/LuauScript/AST/LexerModules/Pos.lua
@@ -1,15 +1,15 @@
 local daPos = {}
 
 function daPos:new(startPos:number, endPos:number, line:number, fileRef:string?)
-    local posInstance = {}
-    setmetatable(posInstance, self)
+        local posInstance = {}
+        setmetatable(posInstance, self)
 
-    posInstance.startPos = startPos
-    posInstance.endPos = endPos
-    posInstance.line = line
-    posInstance.fileRef = fileRef
+        posInstance.startPos = startPos
+        posInstance.endPos = endPos
+        posInstance.line = line
+        posInstance.fileRef = fileRef
 
-    return posInstance
+        return posInstance
 end
 
 return daPos
